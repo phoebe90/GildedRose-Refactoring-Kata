72411f19ec055c9be1c9145224bf5bba10640cee
yaosiang.su <yaosiang.su@104.com.tw>
2020-07-15 18:23:35 +0800

    move behavior to Item class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,53 +19,12 @@ class GildedRose {
         }
 
         if (item.name.equals("Aged Brie")) {
-            upgradeAgedBrie(item);
+            item.upgradeAgedBrie(this);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            upgradeBackstagePasses(item);
+            item.upgradeBackstagePasses(this);
         } else {
-            upgradeNormal(item);
+            item.upgradeNormal(this);
         }
     }
 
-    public void upgradeNormal(Item item) {
-        item.sellIn = item.sellIn - 1;
-        decreaseQuality(item);
-        if (item.sellIn < 0) {
-            decreaseQuality(item);
-        }
-    }
-
-    public void upgradeBackstagePasses(Item item) {
-        item.sellIn = item.sellIn - 1;
-        increaseQuality(item);
-        if (item.sellIn < 10) {
-            increaseQuality(item);
-        }
-        if (item.sellIn < 5) {
-            increaseQuality(item);
-        }
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
-
-    public void upgradeAgedBrie(Item item) {
-        item.sellIn = item.sellIn - 1;
-        increaseQuality(item);
-        if (item.sellIn < 0) {
-            increaseQuality(item);
-        }
-    }
-
-    public void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    public void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,46 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void upgradeNormal(GildedRose gildedRose) {
+        sellIn = sellIn - 1;
+        decreaseQuality();
+        if (sellIn < 0) {
+            decreaseQuality();
+        }
+    }
+
+    public void upgradeBackstagePasses(GildedRose gildedRose) {
+        sellIn = sellIn - 1;
+        increaseQuality();
+        if (sellIn < 10) {
+            increaseQuality();
+        }
+        if (sellIn < 5) {
+            increaseQuality();
+        }
+        if (sellIn < 0) {
+            quality = 0;
+        }
+    }
+
+    public void upgradeAgedBrie(GildedRose gildedRose) {
+        sellIn = sellIn - 1;
+        increaseQuality();
+        if (sellIn < 0) {
+            increaseQuality();
+        }
+    }
+
+    public void decreaseQuality() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
+    }
+
+    public void increaseQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
 }
